887a18242521224fd43bdfadacc6731163b10733
Arthur250-rgb <arthur_l_silveira@estudante.sesisenai.org.br>
2024-08-26 15:26:32 -0300

    primeiro commit

---
+++ Exercicio1.py
@@ -0,0 +1,5 @@
+soma = 0
+#comando para somar 50 números 
+for i in range(1, 51):
+ soma += i 
+ print("Soma:",soma)
---
+++ Exercicio13.py
@@ -0,0 +1,6 @@
+# Loop que percorre a todos os números de 1 a 100 (inclusive)
+for numero in range(1, 101):
+ #Verificar se o número é inteiro
+ if numero % 2 != 0:
+  # Exibir resultado 
+  print(numero,end=" ") 
